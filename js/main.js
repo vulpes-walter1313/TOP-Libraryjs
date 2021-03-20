e8a5d8cf6ef0330c5584e42a7c610912772a87dc
Walter Fritsche <wfritsche95@protonmail.com>
2021-03-20 14:45:37 -0500

    added functionality to various buttons, other changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 let myLibrary = [];
 const bookShelf = document.querySelector('.books-wrapper');
+const addBookBtn = document.querySelector('#add-book-btn');
+const cancelBookBtn = document.querySelector('#cancel-book-btn');
 
-function Book(title, author, numOfPages, readStatus=false) {
+function Book(title="unavailable", author="unknown", numOfPages=0, readStatus=false) {
     // Constructor
     this.title = title;
     this.author = author;
@@ -32,9 +34,12 @@ function formatLongNums(num) {
 }
 function populateShelf() {
     bookShelf.innerHTML = "";
+    let bookIndex = 0;
     myLibrary.forEach(book => {
         let card = document.createElement('div');
         card.classList.add('book-card');
+        // this index data key will help remove books from myLibrary later
+        card.dataset.index = bookIndex;
 
         let bookTitleElem = document.createElement('p');
         bookTitleElem.classList.add('book-title');
@@ -51,26 +56,71 @@ function populateShelf() {
         let bookReadElem = document.createElement('p');
         bookReadElem.classList.add('book-read');
         bookReadElem.textContent = `Has been read? ${book.readStatus}`;
+        // create a div that will house two buttons
+        let btnDiv = document.createElement('div');
+        let readBtn = document.createElement('button');
+        readBtn.classList.add("inbook-btn");
+        readBtn.classList.add("inbook-read-btn");
+        readBtn.textContent = "Read?";
+        readBtn.setAttribute("type", "button");
+        
+        let removeBtn = document.createElement('button');
+        removeBtn.classList.add("inbook-btn");
+        removeBtn.classList.add("inbook-remove-btn");
+        removeBtn.textContent = "Remove";
+        removeBtn.setAttribute("type", "button");
+
+        btnDiv.appendChild(readBtn);
+        btnDiv.appendChild(removeBtn);
 
         card.appendChild(bookTitleElem);
         card.appendChild(bookAuthorElem);
         card.appendChild(bookPagesElem);
         card.appendChild(bookReadElem);
+        card.appendChild(btnDiv);
 
         bookShelf.appendChild(card);
+        bookIndex++;
     });
+}
+
+function handleBookAdd() {
+    const titleBtn = document.querySelector('#book-title-input');
+    const authorBtn = document.querySelector('#book-author-input');
+    const pagesBtn = document.querySelector('#book-pages-input');
+    const readBtn = document.querySelector('#book-read-input');
+
+    const bookTitle = titleBtn.value;
+    const bookAuthor = authorBtn.value;
+    const bookPages = pagesBtn.valueAsNumber;
+    const bookRead = readBtn.checked;
+    // console.log({bookTitle, bookAuthor, bookPages, bookRead});
+    addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRead);
+    // reset the values of all inputs for next submission
+    titleBtn.value = "";
+    authorBtn.value = "";
+    pagesBtn.value = NaN;
+    readBtn.checked = false;
+    populateShelf();
 }
 addBookToLibrary("Harry Potter 1", "J.K.Rowling", 825, true);
 addBookToLibrary("Harry Potter 2", "J.K.Rowling", 1125, true);
-addBookToLibrary("Once Upon A Time", "Author", 9800, true);
-addBookToLibrary("Frankenstein", "Mary Shelly", 742, false);
-addBookToLibrary("Lord of the Flies", "William Golding", 775, false);
-addBookToLibrary("Frankenstein", "Mary Shelly", 742, false);
-addBookToLibrary("Lord of the Flies", "William Golding", 775, false);
+// addBookToLibrary("Once Upon A Time", "Author", 9800, true);
+// addBookToLibrary("Frankenstein", "Mary Shelly", 742, false);
+// addBookToLibrary("Lord of the Flies", "William Golding", 775, false);
+// addBookToLibrary("Frankenstein", "Mary Shelly", 742, false);
+// addBookToLibrary("Lord of the Flies", "William Golding", 775, false);
 
 populateShelf();
+// Add book popup form
 const addBookForm = document.querySelector("#add-book-form");
 const newBookBtn = document.querySelector("#new-book");
 newBookBtn.addEventListener('click', ()=> {
-    addBookForm.classList.toggle("hide");
+    addBookForm.classList.remove("hide");
 });
+
+// close popup form to add book
+cancelBookBtn.addEventListener('click', () => addBookForm.classList.add('hide'));
+
+// Add book to book list from form
+addBookBtn.addEventListener('click', handleBookAdd);

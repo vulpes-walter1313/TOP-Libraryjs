44b9c71e1efbeff5da85a62e24ab052682ec8f7e
Walter Fritsche <wfritsche95@protonmail.com>
2022-10-27 18:11:35 -0500

    added some basic login and logout markup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,34 @@
-import Head from 'next/head';
-import Image from 'next/image'
-import LoginForms from '../components/LoginForms';
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useContext } from "react";
+import LoginForms from "../components/LoginForms";
+import { auth } from "../lib/firebase";
+import { UserContext } from "../lib/UserContext";
+import styles from "../styles/Home.module.css";
+import { signOut } from "firebase/auth";
 
 export default function Home() {
+  const { user } = useContext(UserContext);
   return (
-    <main className='homepage-main'>
+    <main className="homepage-main">
       <Head>
         <title>Library App</title>
       </Head>
       <h1>Welcome To Your Digital Library Index</h1>
-      <p>Log in to get started!</p>
-      <LoginForms />
+      {user ? (
+        <button
+          onClick={() => {
+            console.log("sign out", auth.currentUser?.displayName);
+            signOut(auth);
+          }}
+        >
+          Sign Out
+        </button>
+      ) : (
+        <>
+          <p>Log in to get started!</p>
+          <LoginForms />
+        </>
+      )}
     </main>
-  )
+  );
 }

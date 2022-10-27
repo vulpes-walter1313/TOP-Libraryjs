3c5a0b28a1d4d2093fd6a881f12e9e97f35542b7
Walter Fritsche <wfritsche95@protonmail.com>
2022-10-27 11:49:14 -0500

    base start to work on login form

---
+++ components/LoginForms.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function LoginForms() {
+  return (
+    <div>
+      <h2>Login to Continue</h2>
+      <button>Login with Google</button>
+      <form>
+        
+      </form>
+    </div>
+  )
+}

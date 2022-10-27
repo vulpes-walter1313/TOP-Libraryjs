e75fed97fe968359228434f4a051201381abdd6b
Walter Fritsche <wfritsche95@protonmail.com>
2022-10-27 17:27:56 -0500

    added some basic components to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import Head from 'next/head';
 import Image from 'next/image'
+import LoginForms from '../components/LoginForms';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <main className='homepage-main'>
       <Head>
         <title>Library App</title>
       </Head>
       <h1>Welcome To Your Digital Library Index</h1>
-    </div>
+      <p>Log in to get started!</p>
+      <LoginForms />
+    </main>
   )
 }

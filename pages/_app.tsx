59cece726a49f89ab4cf2b07a7631b0dac9d6ae7
Walter Fritsche <wfritsche95@protonmail.com>
2022-10-27 18:10:05 -0500

    added user into context provider for all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { UserContext } from "../lib/UserContext";
+import { auth } from "../lib/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [user] = useAuthState(auth);
+  return (
+    <UserContext.Provider value={{ user }}>
+      <Component {...pageProps} />
+    </UserContext.Provider>
+  );
 }

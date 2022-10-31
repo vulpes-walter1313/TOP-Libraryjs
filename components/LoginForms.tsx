9c6fc114fc1fc8fec9af7fe0ceee6f0d47e911c1
Walter Fritsche <wfritsche95@protonmail.com>
2022-10-31 17:40:51 -0500

    added login and registration forms but no auth action

--- components/LoginForms.tsx
+++ components/LoginForms.tsx
@@ -1,17 +1,43 @@
 import { signInWithPopup } from "firebase/auth";
 import React from "react";
+import { useForm } from "react-hook-form";
 import { auth, googleAuthProvider } from "../lib/firebase";
+import styles from "./LoginForms.module.css";
+import RegisterAccount from "./RegisterAccount";
 
+ export interface LoginFormInput {
+  email: string;
+  password: string;
+}
 export default function LoginForms() {
+  const {register, handleSubmit, formState: { errors }} = useForm<LoginFormInput>();
+
   async function signInWithGoogle() {
     await signInWithPopup(auth, googleAuthProvider);
   }
-
+  function submitData(data: LoginFormInput) {
+    console.log(data);
+  }
   return (
     <div>
-      <h2>Login to Continue</h2>
       <button onClick={signInWithGoogle}>Login with Google</button>
-      <form></form>
+      <form onSubmit={handleSubmit(submitData)}>
+        <p className={styles.title}>Log in with Email and Password</p>
+        <div className={styles.formItem}>
+          <label htmlFor="email">Email:</label>
+          <input {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })} id="email"/>
+          {errors.email?.type === "required" && <p className={styles.error}>Email is required!</p>}
+          {errors.email?.type === "pattern" && <p className={styles.error}>Provide a valid email</p>}
+        </div>
+        <div className={styles.formItem}>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" {...register("password", {minLength: 3, maxLength: 25 })}/>
+          {errors.password?.type === "minLength" && <p className={styles.error}>The password is too small</p>}
+          {errors.password?.type === "maxLength" && <p className={styles.error}>The password is too long</p>}
+        </div>
+        <input type="submit"/>
+      </form>
+      <RegisterAccount />
     </div>
   );
 }

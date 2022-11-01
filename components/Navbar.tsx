e912a689b06eec4b26e0246e98d90cb1ac697d95
Walter Fritsche <wfritsche95@protonmail.com>
2022-11-01 14:12:08 -0500

    Basic Navbar and styles

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+ import React from 'react';
+ import styles from "./Navbar.module.css";
+ 
+ export default function Navbar() {
+   return (
+     <nav className={styles.navbarContainer}>
+      <p className={styles.title}>Library App</p>
+     </nav>
+   )
+ }
+ 

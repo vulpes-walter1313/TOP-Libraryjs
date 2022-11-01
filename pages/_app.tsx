2357e362de66bc1da9f66b430a38fafe4aed1955
Walter Fritsche <wfritsche95@protonmail.com>
2022-11-01 14:13:21 -0500

    added Navbar to all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import type { AppProps } from "next/app";
 import { UserContext } from "../lib/UserContext";
 import { auth } from "../lib/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import Navbar from "../components/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [user] = useAuthState(auth);
   return (
     <UserContext.Provider value={{ user }}>
+      <Navbar />
       <Component {...pageProps} />
     </UserContext.Provider>
   );

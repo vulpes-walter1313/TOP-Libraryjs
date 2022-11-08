2a1d3d99e48f34837ec08ace63d7c8bae807ec4c
Walter Fritsche <wfritsche95@protonmail.com>
2022-11-08 14:47:54 -0600

    added bookshelf component behind authcheck

--- pages/library.tsx
+++ pages/library.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import AuthCheck from '../components/AuthCheck'
+import Bookshelf from '../components/Bookshelf';
 
 export default function LibraryPage() {
   return (
     <AuthCheck>
-      <>
-        <h1>Welcome to your library</h1>
-        <div>LibraryPage</div>
-      </>
+      <Bookshelf />
     </AuthCheck>
   );
 }

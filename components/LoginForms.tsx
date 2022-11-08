55bb674d0e344cb98a08ce14cd797cfe7a64be39
Walter Fritsche <wfritsche95@protonmail.com>
2022-11-07 21:58:35 -0600

    changed to only see either login or create account form

--- components/LoginForms.tsx
+++ components/LoginForms.tsx
@@ -1,6 +1,6 @@
 import { signInWithPopup } from "firebase/auth";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { auth, googleAuthProvider } from "../lib/firebase";
 import ErrorMessage from "./ErrorMessage";
@@ -13,6 +13,7 @@ import RegisterAccount from "./RegisterAccount";
 }
 export default function LoginForms() {
   const {register, handleSubmit, formState: { errors }} = useForm<LoginFormInput>();
+  const [wantsToRegister, setWantsToRegister] = useState(false);
   const router = useRouter();
 
   async function signInWithGoogle() {
@@ -27,23 +28,30 @@ export default function LoginForms() {
       <button className={styles.googleBtn} onClick={signInWithGoogle}>
         <img  src="/google.png" style={{width: '40px', height: '40px'}}/>
         Login with Google</button>
-      <form className={styles.form} onSubmit={handleSubmit(submitData)}>
-        <p className={styles.title}>Log in with Email and Password</p>
-        <div className={styles.formItem}>
-          <label htmlFor="email">Email:</label>
-          <input {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })} id="email"/>
-          {errors.email?.type === "required" && <ErrorMessage>Email is required!</ErrorMessage>}
-          {errors.email?.type === "pattern" && <ErrorMessage>Provide a valid email</ErrorMessage>}
-        </div>
-        <div className={styles.formItem}>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" {...register("password", {minLength: 3, maxLength: 25 })}/>
-          {errors.password?.type === "minLength" && <ErrorMessage>The password is too small</ErrorMessage>}
-          {errors.password?.type === "maxLength" && <ErrorMessage>The password is too long</ErrorMessage>}
-        </div>
-        <input className={styles.submitBtn} type="submit"/>
-      </form>
-      <RegisterAccount />
+      <button onClick={() => setWantsToRegister(!wantsToRegister)}>{wantsToRegister === false ? "Don't Have an Account?" : "Already Registered?"}</button>
+        {
+          wantsToRegister === false ? (
+          <form className={styles.form} onSubmit={handleSubmit(submitData)}>
+            <p className={styles.title}>Log in with Email and Password</p>
+            <div className={styles.formItem}>
+              <label htmlFor="email">Email:</label>
+              <input {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })} id="email"/>
+              {errors.email?.type === "required" && <ErrorMessage>Email is required!</ErrorMessage>}
+              {errors.email?.type === "pattern" && <ErrorMessage>Provide a valid email</ErrorMessage>}
+            </div>
+            <div className={styles.formItem}>
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" {...register("password", {minLength: 3, maxLength: 25 })}/>
+              {errors.password?.type === "minLength" && <ErrorMessage>The password is too small</ErrorMessage>}
+              {errors.password?.type === "maxLength" && <ErrorMessage>The password is too long</ErrorMessage>}
+            </div>
+            <input className={styles.submitBtn} type="submit"/>
+          </form>
+          ) : null
+        }
+      {
+        wantsToRegister ? <RegisterAccount /> : null
+      }
     </div>
   );
 }

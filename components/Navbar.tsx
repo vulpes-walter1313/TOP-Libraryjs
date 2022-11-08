68104fdf18ddd8c1b18ecaf940f19a4803c6f3d7
Walter Fritsche <wfritsche95@protonmail.com>
2022-11-07 22:00:17 -0600

    added link to homepage

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,13 @@
- import React from 'react';
+ import Link from 'next/link';
+import React from 'react';
  import styles from "./Navbar.module.css";
  
  export default function Navbar() {
    return (
      <nav className={styles.navbarContainer}>
-      <p className={styles.title}>Library App</p>
+      <Link href="/">
+        <p className={styles.title}>Library App</p>
+      </Link>
      </nav>
    )
  }

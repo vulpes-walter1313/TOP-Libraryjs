4e616e6a546abad3c1ed172ea1fc564047a0eed5
Walter Fritsche <wfritsche95@protonmail.com>
2022-11-07 22:00:53 -0600

    implemented an auth check component to prevent unauthorized views

---
+++ components/AuthCheck.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import React, { useContext } from 'react'
+import { UserContext } from '../lib/UserContext'
+
+export default function AuthCheck(props: any) {
+  const { user } = useContext(UserContext);
+  console.log("In AuthCheck", user);
+  if (user) {
+    return props.children;
+  } else {
+    return (
+      <div>
+        <h1>You're not signed in!</h1>
+        <Link href="/">
+          <p>Login</p>
+        </Link>
+      </div>
+    )
+  }
+}

f0b7d3bc0c7561c66539c22b7294a457b4d6fcfc
Walter Fritsche <wfritsche95@protonmail.com>
2022-11-09 14:23:08 -0600

    updated to correct property names for documents from Firestore

--- components/Bookshelf.tsx
+++ components/Bookshelf.tsx
@@ -9,8 +9,8 @@ import styles from "./Bookshelf.module.css";
 export default function Bookshelf() {
   const {user} = useContext(UserContext);
   const [values, loading, error] = useCollectionData(collection(firestore, 'users', (user as User).uid, 'books'));
-  // const [selectedBook, setSelectedBook] = useState<{Title: string; Author: string; pages: number; read: boolean;} | null>(null)
   const [selectedBook, setSelectedBook] = useState<DocumentData | undefined>();
+
   console.log(values);
   return (
     <main className={styles.mainContainer}>
@@ -18,15 +18,14 @@ export default function Bookshelf() {
       {error ? <div>oops, Looks like there is an error chief...</div> : null}
       <div className={styles.displayContainer}>
         <ul>
-          {values ? values.map(val => <li className={styles.listItem} key={val.id} onClick={() => setSelectedBook(val)}>Title: {val.Title}. Author: {val.Author}</li>) : null}
+          {values ? values.map(val => <li className={styles.listItem} key={val.id} onClick={() => setSelectedBook(val)}>Title: {val.title}. Author: {val.author}</li>) : null}
         </ul>
         <div className={styles.selectedContainer}>
-          <p className={styles.selectedTitle}>{selectedBook?.Title}</p>
-          <p>by {selectedBook?.Author}</p>
+          <p className={styles.selectedTitle}>{selectedBook?.title}</p>
+          <p>by {selectedBook?.author}</p>
           <p>Pages: {selectedBook?.pages}</p>
           <p>Read: {selectedBook?.read.toString()}</p>
           <button type='button'>remove</button>
-
         </div>
       </div>
     </main>

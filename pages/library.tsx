42c1ff0728575243331f09138c174c2eb1d9b1a3
Walter Fritsche <wfritsche95@protonmail.com>
2022-11-10 14:12:56 -0600

    added functional submit logic

--- pages/library.tsx
+++ pages/library.tsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import AuthCheck from '../components/AuthCheck'
 import Bookshelf from '../components/Bookshelf';
 import { FiPlus } from "react-icons/fi";
 import styles from "../styles/library.module.css";
 import { useForm } from 'react-hook-form';
+import { setDoc, collection, doc, serverTimestamp, updateDoc } from 'firebase/firestore';
+import { firestore } from '../lib/firebase';
+import { UserContext } from '../lib/UserContext';
+import { User } from 'firebase/auth';
 
 interface AddBookFormInput {
   title: string;
@@ -13,16 +17,36 @@ interface AddBookFormInput {
 }
 
 export default function LibraryPage() {
+  const {user} = useContext(UserContext);
   const [showAddBook, setShowAddBook] = useState(true);
-  const {register, handleSubmit, formState: { errors }} = useForm<AddBookFormInput>()
-  
+  const {register, handleSubmit, formState: { errors, isSubmitSuccessful }, reset} = useForm<AddBookFormInput>()
+
+  const submitHandle = async (data: AddBookFormInput) => {
+    const docRef = await doc(collection(firestore, "users", (user as User).uid, 'books'))
+    await setDoc(docRef, {
+      title: data.title,
+      author: data.author,
+      pages: data.pages,
+      read: data.read,
+      addedAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+      id: docRef.id
+    });
+    console.log("Book added with ID: ", docRef.id);
+  };
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+      setShowAddBook(false);
+    }
+  }, [isSubmitSuccessful]);
   return (
     <AuthCheck>
       <>
         <Bookshelf />
         {showAddBook ? (
           <div className={styles.addBookFormContainer}>
-            <form onSubmit={handleSubmit((data) => console.log(data))}>
+            <form onSubmit={handleSubmit(submitHandle)}>
               <label htmlFor='title'>Book Title</label>
               <input type="text" {...register("title")}/>
               <label htmlFor='author'>Author</label>

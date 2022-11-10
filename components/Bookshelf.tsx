dc11f528c56e4b1a216600ef018c1e900bb314da
Walter Fritsche <wfritsche95@protonmail.com>
2022-11-10 14:57:15 -0600

    prettier formatted and added some classNames for styling

--- components/Bookshelf.tsx
+++ components/Bookshelf.tsx
@@ -1,14 +1,18 @@
-import React, { useContext, useState } from 'react';
-import { UserContext } from '../lib/UserContext';
-import { useCollectionData} from "react-firebase-hooks/firestore";
-import { collection, deleteDoc, doc, DocumentData } from 'firebase/firestore';
-import { firestore } from '../lib/firebase';
-import { User } from 'firebase/auth';
+import React, { useContext, useState } from "react";
+import { UserContext } from "../lib/UserContext";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { collection, deleteDoc, doc, DocumentData } from "firebase/firestore";
+import { firestore } from "../lib/firebase";
+import { User } from "firebase/auth";
 import styles from "./Bookshelf.module.css";
 
+// React.memo here is ensuring the bookshelf component doesn't rerender when
+// the addBookBtn is clicked on the Library page component
 export default React.memo(function Bookshelf() {
-  const {user} = useContext(UserContext);
-  const [values, loading, error] = useCollectionData(collection(firestore, 'users', (user as User).uid, 'books'));
+  const { user } = useContext(UserContext);
+  const [values, loading, error] = useCollectionData(
+    collection(firestore, "users", (user as User).uid, "books")
+  );
   const [selectedBook, setSelectedBook] = useState<DocumentData | undefined>();
 
   console.log(values);
@@ -18,25 +22,54 @@ export default React.memo(function Bookshelf() {
       {error ? <div>oops, Looks like there is an error chief...</div> : null}
       <div className={styles.displayContainer}>
         <ul>
-          {values ? values.map(val => <li className={styles.listItem} key={val.id} onClick={() => setSelectedBook(val)}>Title: {val.title}. Author: {val.author}</li>) : null}
+          {values
+            ? values.map((val) => (
+                <li
+                  className={styles.listItem}
+                  key={val.id}
+                  onClick={() => setSelectedBook(val)}
+                >
+                  <div>
+                  <span className={styles.listItemTitle}>{val.title}</span>
+                  <span> by {val.author}</span>
+                  </div>
+                  <span>{val.read ? "Read" : "Not Read"}</span>
+                </li>
+              ))
+            : null}
         </ul>
         <div className={styles.selectedContainer}>
           {selectedBook ? (
-          <>
-            <p className={styles.selectedTitle}>{selectedBook?.title}</p>
-            <p>by {selectedBook?.author}</p>
-            <p>Pages: {selectedBook?.pages}</p>
-            <p>Read: {selectedBook?.read.toString()}</p>
-            <button type='button' onClick={ async () => {
-              if (selectedBook) {
-                await deleteDoc(doc(firestore, "users", (user as User).uid, "books", selectedBook.id))
-                setSelectedBook(undefined);
-              }
-            }}>remove</button>
-          </>
-          ) : <p>please select a book</p>}
+            <>
+              <p className={styles.selectedTitle}>{selectedBook?.title}</p>
+              <p className={styles.selectedAuthor}>by {selectedBook?.author}</p>
+              <p>Pages: {selectedBook?.pages}</p>
+              <p>Read: {selectedBook?.read.toString()}</p>
+              <button
+                type="button"
+                onClick={async () => {
+                  if (selectedBook) {
+                    await deleteDoc(
+                      doc(
+                        firestore,
+                        "users",
+                        (user as User).uid,
+                        "books",
+                        selectedBook.id
+                      )
+                    );
+                    setSelectedBook(undefined);
+                  }
+                }}
+              >
+                remove
+              </button>
+            </>
+          ) : (
+            <p>please select a book</p>
+          )}
         </div>
       </div>
     </main>
-  )
-})
+  );
+});

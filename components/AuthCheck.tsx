b33d7ee383f9013c95a751ff4067d17f379e16b8
Walter Fritsche <wfritsche95@protonmail.com>
2022-11-14 16:17:24 -0600

    added basic inline-styles for fallback

--- components/AuthCheck.tsx
+++ components/AuthCheck.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
-import React, { useContext } from 'react'
-import { UserContext } from '../lib/UserContext'
+import Link from "next/link";
+import React, { useContext } from "react";
+import { UserContext } from "../lib/UserContext";
 
 export default function AuthCheck(props: any) {
   const { user } = useContext(UserContext);
@@ -8,12 +8,33 @@ export default function AuthCheck(props: any) {
     return props.children;
   } else {
     return (
-      <div>
-        <h1>You're not signed in!</h1>
-        <Link href="/">
-          <p>Login</p>
+      <div
+        style={{
+          width: "100vw",
+          height: "calc(100vh - 3rem)",
+          backgroundColor: "var(--main)",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "1.5rem"
+        }}
+      >
+        <h1 style={{ color: "var(--black)" }}>You're not signed in!</h1>
+        <Link href="/" style={{textDecoration: "none"}}>
+          <p
+            style={{
+              backgroundColor: "var(--white)",
+              padding: "1rem",
+              borderRadius: "10px",
+              fontSize: "1rem",
+              color: "var(--black)",
+            }}
+          >
+            Login
+          </p>
         </Link>
       </div>
-    )
+    );
   }
 }

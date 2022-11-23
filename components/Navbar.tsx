2040fcb8d339937d460eef598634a577f607b396
Walter Fritsche <wfritsche95@protonmail.com>
2022-11-23 16:02:25 -0600

    changed title to icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,10 @@
- import { signOut } from 'firebase/auth';
+import { signOut } from 'firebase/auth';
 import Link from 'next/link';
 import React, { useContext } from 'react';
 import { auth } from '../lib/firebase';
 import { UserContext } from '../lib/UserContext';
- import styles from "./Navbar.module.css";
+import styles from "./Navbar.module.css";
+import {IoLibrarySharp} from "react-icons/io5";
  
  export default function Navbar() {
    const {user} = useContext(UserContext);
@@ -11,7 +12,7 @@ import { UserContext } from '../lib/UserContext';
    return (
      <nav className={styles.navbarContainer}>
       <Link href="/">
-        <p className={styles.title}>Library App</p>
+        <p className={styles.title}><IoLibrarySharp /></p> 
       </Link>
       <ul className={styles.navlinks}>
         <Link href="/">
